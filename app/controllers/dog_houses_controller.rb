c5fc593697147da1331f9d8133dfbde79ea6ff55
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-10-02 14:32:44 +0300

    Done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,6 +1,17 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
